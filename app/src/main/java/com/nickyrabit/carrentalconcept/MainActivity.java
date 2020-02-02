acaba8e334cab4a5066cefee5cfd4694f3b41393
Nicky Ngailo <nickyrabit@gmail.com>
2020-02-03 00:33:17 +0300

    adding the imageswither

--- app/src/main/java/com/nickyrabit/carrentalconcept/MainActivity.java
+++ app/src/main/java/com/nickyrabit/carrentalconcept/MainActivity.java
@@ -3,12 +3,64 @@ package com.nickyrabit.carrentalconcept;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
-
+     ImageSwitcher imageSwitcher;
+     boolean firstImage;
+Button button;
+    private Handler imageSwitchHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageSwitcher = findViewById(R.id.imageSwitcherView);
+
+
+        // Set the ViewFactory of the ImageSwitcher that will create ImageView object when asked
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+
+            public View makeView() {
+// TODO Auto-generated method stub
+
+// Create a new ImageView and set it's properties
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+                return imageView;
+            }
+        });
+
+        firstImage = true;
+
+        button = findViewById(R.id.button);
+        Animation imgAnimationIn =  AnimationUtils.
+                loadAnimation(this,   R.anim.in_animation);
+        imageSwitcher.setInAnimation(imgAnimationIn);
+
+        Animation imgAnimationOut =  AnimationUtils.
+                loadAnimation(this,   R.anim.out_animation);
+        imageSwitcher.setOutAnimation(imgAnimationOut);
+
+
+button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        imageSwitcher.setImageDrawable(getResources().getDrawable(R.drawable.audi_a8));
+
     }
+});
+
+
+   }
+
+
 }
