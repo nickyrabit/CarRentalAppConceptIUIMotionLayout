63346e0f2cd838257c2112b2ec2bba6500dd570c
Nicky Ngailo <nickyrabit@gmail.com>
2020-02-03 01:06:58 +0300

    adding tiker

--- app/src/main/java/com/nickyrabit/carrentalconcept/MainActivity.java
+++ app/src/main/java/com/nickyrabit/carrentalconcept/MainActivity.java
@@ -13,10 +13,14 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ViewSwitcher;
 
+import com.shawnlin.numberpicker.NumberPicker;
+
 public class MainActivity extends AppCompatActivity {
      ImageSwitcher imageSwitcher;
      boolean firstImage;
 Button button;
+    NumberPicker numberPicker;
+
     private Handler imageSwitchHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,7 @@ Button button;
         setContentView(R.layout.activity_main);
         imageSwitcher = findViewById(R.id.imageSwitcherView);
 
+        numberPicker = (NumberPicker) findViewById(R.id.horizontal_number_picker);
 
         // Set the ViewFactory of the ImageSwitcher that will create ImageView object when asked
         imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
@@ -49,6 +54,22 @@ Button button;
         Animation imgAnimationOut =  AnimationUtils.
                 loadAnimation(this,   R.anim.out_animation);
         imageSwitcher.setOutAnimation(imgAnimationOut);
+
+
+
+
+
+
+        // Using string values
+// IMPORTANT! setMinValue to 1 and call setDisplayedValues after setMinValue and setMaxValue
+        String[] data = {"4 Seater", "5 Seater", "7 Seater", "8 Seater"};
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(data.length);
+        numberPicker.setDisplayedValues(data);
+        numberPicker.setValue(2);
+
+
+
 
 
 button.setOnClickListener(new View.OnClickListener() {
